26c097cf2f247628c87f5b86d6a4c2bbf2c52822
mathewgeola <mathewgeola@gmail.com>
2025-12-08 10:17:34 +0800

    version = "v0.0.2"

--- tests/test_downloader.py
+++ tests/test_downloader.py
@@ -1,5 +1,18 @@
+import downloader
+
+
 def test_downloader():
-    pass
+    file_path = downloader.Downloader().download(
+        "https://www.baidu.com/",
+        use_cache=True, use_tqdm=False
+    )
+    print(file_path)
+
+    file_path = downloader.Downloader().download(
+        "https://www.baidu.com/img/PCtm_d9c8750bed0b3c7d089fa7d55720d6cf.png",
+        use_cache=False, use_tqdm=True
+    )
+    print(file_path)
 
 
 if __name__ == '__main__':
